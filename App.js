8c0f3ac0f2ec71df924caafe0c74e5d55dd3451d
luckrafael <lrs.rafael@hotmail.com>
2022-02-11 21:39:18 -0300

    starting project and adding components

---
+++ App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import params from './src/params';
+import Field from './src/components/Field';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines! </Text>
+        <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
+        <Field flagged />
+        <Field flagged opened />
+        <Field flagged opened />
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  instructions: {
+    fontSize: 18
+  }
+})
