30ed88f2afd38199a7a3329d16427777b9c6e93e
luckrafael <lrs.rafael@hotmail.com>
2022-02-11 22:34:29 -0300

    adding board

---
+++ src/components/MineField.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Field from './Field';
+
+// import { createMineBoard } from "../functions";
+
+
+export default props => {
+    const rows = props.board.map((row, r) => {
+        const columns = row.map((field, c) => {
+            return <Field {...field} key={c} />
+        })
+        return <View key={r}
+            style={{ flexDirection: 'row' }}>
+            {columns}
+        </View>
+    })
+    return <View style={styles.container}>{rows}</View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#EEE'
+    }
+})
